01393860c7c1317676eb36d237552635bff3fef7
Momosan09 <80794773+Momosan09@users.noreply.github.com>
2021-07-25 00:01:11 -0300

    Add files via upload

--- calculadorarotao/main.js
+++ calculadorarotao/main.js
@@ -1,17 +1,16 @@
 console.log("Bienvenido");
 
+
 let getData = function(){
     let First = document.getElementById("first").value;         //Llama a "first" y lo asigna a la variable First (".value" sirve para asignar el valor a la variable)
     let Second = document.getElementById("second").value;       //Llama a "second" y lo asigna a la variable Second (".value" sirve para asignar el valor a la variable)
     let Last = document.getElementById("last").value;           //Llama a "last" y lo asigna a la variable Last (".value" sirve para asignar el valor a la variable)
-
 
     if (First == ""){
         document.getElementById("first").focus();
     }
 
 
-
-    console.log(First+" "+Second+" "+Last);
+    console.log(First+Second+Last);
 
 }
