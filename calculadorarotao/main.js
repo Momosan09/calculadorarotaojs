38fec064c54c59fd0b8810de968fdb7e4b8f06db
Momosan09 <80794773+Momosan09@users.noreply.github.com>
2021-07-25 21:01:56 -0300

    Add files via upload

--- calculadorarotao/main.js
+++ calculadorarotao/main.js
@@ -4,7 +4,6 @@ console.log("Bienvenido");
 let getData = function(){
     let First = parseInt( document.getElementById("first").value);         //Llama a "first" y lo asigna a la variable First (".value" sirve para asignar el valor a la variable)
     let Second = parseInt( document.getElementById("second").value);       //Llama a "second" y lo asigna a la variable Second (".value" sirve para asignar el valor a la variable)
-    let Seconds = parseInt( document.getElementById("seconds").value);       //Llama a "seconds" y lo asigna a la variable Seconds (".value" sirve para asignar el valor a la variable)
     let Last = parseInt( document.getElementById("last").value);           //Llama a "last" y lo asigna a la variable Last (".value" sirve para asignar el valor a la variable)
 
     //parseint convierte el string a numero entero
@@ -12,8 +11,9 @@ let getData = function(){
     if (First == ""){
         document.getElementById("first").focus();
     }
+let resultado = First + Second + Last;
 
-    console.log( First+Second+Last);
+    console.log( "El total es: " + resultado );
 
 
 }
