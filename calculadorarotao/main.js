98350c06164f446b639e34fccfd5eed13119cc4b
Momosan09 <80794773+Momosan09@users.noreply.github.com>
2021-07-25 21:26:41 -0300

    Add files via upload

--- calculadorarotao/main.js
+++ calculadorarotao/main.js
@@ -3,7 +3,7 @@ console.log("Bienvenido");
 
 let getData = function(){
     let First = parseInt( document.getElementById("first").value);         //Llama a "first" y lo asigna a la variable First (".value" sirve para asignar el valor a la variable)
-    let Second = parseInt( document.getElementById("second").value);       //Llama a "second" y lo asigna a la variable Second (".value" sirve para asignar el valor a la variable)
+    let Second = parseInt( document.getElementById("second").checked);       //Llama a "second" y lo asigna a la variable Second (".value" sirve para asignar el valor a la variable)
     let Last = parseInt( document.getElementById("last").value);           //Llama a "last" y lo asigna a la variable Last (".value" sirve para asignar el valor a la variable)
 
     //parseint convierte el string a numero entero
---
+++ calculadorarotao/main2.js
@@ -0,0 +1,13 @@
+let ShowData = function (){
+
+    let First = parseInt( document.getElementById("first").value);
+
+    let Second = parseInt( document.getElementById("americano").checked);
+    let Second = parseInt( document.getElementById("presilla").checked);
+
+
+
+let resultado = First
+    console.log(Second);
+
+}
