57175a52fb77410fdc41e6d095f62b5ec957f49f
Momosan09 <80794773+Momosan09@users.noreply.github.com>
2021-07-25 21:46:15 -0300

    Add files via upload

--- calculadorarotao/main2.js
+++ calculadorarotao/main2.js
@@ -2,12 +2,22 @@ let ShowData = function (){
 
     let First = parseInt( document.getElementById("first").value);
 
-    let Second = parseInt( document.getElementById("americano").checked);
-    let Second = parseInt( document.getElementById("presilla").checked);
+    let M1 = document.getElementById("americano").checked;
 
+    let M2 = document.getElementById("presilla").checked;
 
+    if(M1 ==  true){
+       americano = First * 1.5;
+        console.log("!"+americano);
+    }
+
+    if(M2 == true){
+        presilla= First * 2;
+        console.log("!"+presilla);
+    }
 
 let resultado = First
-    console.log(Second);
-
+    console.log(resultado);
+    console.log(M1);
+    console.log(M2);
 }
