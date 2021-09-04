05ece8279ebadff942a830d63956f267c9ab1581
Momosan09 <80794773+Momosan09@users.noreply.github.com>
2021-09-03 23:28:22 -0300

    no terminada
    La prueba de la funcion para reducir codigo

---
+++ funcion.js
@@ -0,0 +1,24 @@
+let ShowData = function (){
+
+    let array = [
+    M0 = document.getElementById("0").checked, 1,
+    M1 = document.getElementById("1").checked, 2,
+    M2 = document.getElementById("2").checked, 3];
+
+    let verdaero=[];
+
+for(i=0; i< array.length; i++){
+    console.log(array[i]);
+    if(array[i]== true){
+        verdaero.push(array[i]);
+    }
+}
+
+console.log(verdaero);
+
+
+
+
+
+
+}
