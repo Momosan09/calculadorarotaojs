9b9f708e6b94e63021436cd705d00e8bc7b881e2
Momosan09 <80794773+Momosan09@users.noreply.github.com>
2021-09-05 15:31:07 -0300

    anda a medias
    falta hacer que la funcion suma ande pero lo mas dificil que es la matriz ya esta

--- funcion.js
+++ funcion.js
@@ -1,24 +1,69 @@
-let ShowData = function (){
-
-    let array = [
-    M0 = document.getElementById("0").checked, 1,
-    M1 = document.getElementById("1").checked, 2,
-    M2 = document.getElementById("2").checked, 3];
-
-    let verdaero=[];
-
-for(i=0; i< array.length; i++){
-    console.log(array[i]);
-    if(array[i]== true){
-        verdaero.push(array[i]);
-    }
+arrayp = ["a","B"];
+arrayv = [];
+console.log(arrayp);
+console.log(arrayv);
+if( arrayp[0] === "a"){
+arrayv.push(arrayp);
+console.log(arrayv);
 }
 
-console.log(verdaero);
+
+let arr = [1,2];
+
+function suma (a,b){
+    console.log(a + b);
+}
+
+/* arr.forEach(function(element){
+    console.log(element)
+    suma(a,b);    
+    } 
+);
+ */
 
 
 
 
 
+let resultado = suma(arr);
+suma(arr);
 
+console.log(resultado);
+
+arrayp.forEach(function(element){
+    console.log(element);
+});
+
+let ShowData = function (){
+
+    let verdadero=[];
+
+    let array = [
+    [document.getElementById("0").checked, 1,2],
+    [document.getElementById("1").checked, 2,3],
+    [document.getElementById("2").checked, 3,4]];
+
+array.forEach(function(element){
+    console.log(element);
+    console.log("");
+        element.forEach(function(elemento){
+            console.log(elemento);
+            if(elemento === true){
+                console.log("anda");
+                    verdadero.push(...element); /* Estos tres puntos sirven para descomponer el array, si los saco se formaria una matriz y no es lo que necesito */
+        }
+    })
+});
+
+console.log(verdadero);
+verdadero.shift();
+console.log(verdadero);
+
+function suma(a, b){
+    return a + b;
+
+}
+const saludo = suma(verdadero);
+
+console.log(saludo);
 }
