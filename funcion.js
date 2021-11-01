ab9d4a40af5fd8f6e54b020e4fe0708e80dbb210
Momosan09 <80794773+Momosan09@users.noreply.github.com>
2021-10-31 21:45:49 -0300

    achica codigo

--- funcion.js
+++ funcion.js
@@ -1,45 +1,9 @@
-arrayp = ["a","B"];
-arrayv = [];
-console.log(arrayp);
-console.log(arrayv);
-if( arrayp[0] === "a"){
-arrayv.push(arrayp);
-console.log(arrayv);
-}
-
-
-let arr = [1,2];
-
-function suma (a,b){
-    console.log(a + b);
-}
-
-/* arr.forEach(function(element){
-    console.log(element)
-    suma(a,b);    
-    } 
-);
- */
-
-
-
-
-
-let resultado = suma(arr);
-suma(arr);
-
-console.log(resultado);
-
-arrayp.forEach(function(element){
-    console.log(element);
-});
-
 let ShowData = function (){
 
     let verdadero=[];
 
     let array = [
-    [document.getElementById("0").checked, 1,2],
+    [document.getElementById("0").checked, 1,2, "Americano"],
     [document.getElementById("1").checked, 2,3],
     [document.getElementById("2").checked, 3,4]];
 
@@ -52,18 +16,21 @@ array.forEach(function(element){
                 console.log("anda");
                     verdadero.push(...element); /* Estos tres puntos sirven para descomponer el array, si los saco se formaria una matriz y no es lo que necesito */
         }
-    })
+    });
 });
 
 console.log(verdadero);
 verdadero.shift();
 console.log(verdadero);
 
-function suma(a, b){
-    return a + b;
-
+function suma(a,b,c){
+    let result = a+b;
+    console.log(result);
+    
+    this.c = verdadero[2];
+    console.log(this.c);
 }
-const saludo = suma(verdadero);
 
-console.log(saludo);
+suma(verdadero[0],verdadero[1]);
+
 }
